b0b94cf2257cd95b981fb874d3ec5dba49e1f250
diogosampaio@ua.pt <diogosampaio@ua.pt>
2026-04-11 18:37:52 +0100

    um

--- src/main/java/forms/Projetos.java
+++ src/main/java/forms/Projetos.java
@@ -1,4 +1,19 @@
 package forms;
 
+import javax.swing.*;
+
 public class Projetos {
-}
+    private JPanel contentPane;
+    private JButton CriarProjeto;
+    private JButton EditarProjeto;
+    private JButton EliminarProjeto;
+    private JButton CarregarEstado;
+    private JButton GuardarEstado;
+    private JList list1;
+    private JButton carregarButton;
+    private JButton guardarButton;
+    private JButton criarButton;
+    private JButton eliminarButton;
+    private JButton visualizarButton;
+    private JButton editarButton;
+};
--- src/main/java/forms/TarefasEquipas.java
+++ src/main/java/forms/TarefasEquipas.java
@@ -1,4 +1,14 @@
 package forms;
+import javax.swing.*;
 
 public class TarefasEquipas {
+    private JPanel contentPane;
+    private JTabbedPane tabbedPane1;
+    private JButton eliminarButton;
+    private JButton editarButton;
+    private JButton adicionarButton;
+    private JButton adicionarMembroButton;
+    private JButton removerButton;
+    private JButton atribuirButton;
+    private JList list1;
 }
